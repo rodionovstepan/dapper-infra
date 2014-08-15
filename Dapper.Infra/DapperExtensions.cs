90759765336370288ac0573a91ec2f7ef4b77c43
rodionovstepan <rodionov.stepan@byndyusoft.com>
2014-08-15 16:59:06 +0600

    add query object and extensions

---
+++ Dapper.Infra/DapperExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Dapper.Infra
+{
+    using System.Collections.Generic;
+    using System.Data;
+
+    public static class DapperExtensions
+    {
+        public static IEnumerable<T> Query<T>(this IDbConnection connection, QueryObject queryObject)
+        {
+            return connection.Query<T>(queryObject.Sql, queryObject.QueryParams);
+        }
+
+        public static int Execute(this IDbConnection connection, QueryObject queryObject)
+        {
+            return connection.Execute(queryObject.Sql, queryObject.QueryParams);
+        }
+    }
+}
---
+++ Dapper.Infra/QueryObject.cs
@@ -0,0 +1,14 @@
+﻿namespace Dapper.Infra
+{
+    public class QueryObject
+    {
+        public QueryObject(string sql, object queryParams = null)
+        {
+            Sql = sql;
+            QueryParams = queryParams;
+        }
+
+        public string Sql { get; private set; }
+        public object QueryParams { get; private set; }
+    }
+}
